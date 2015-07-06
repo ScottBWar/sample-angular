c8cc5298966e797ec4dd42332dc4db0b73fd8a95
Scott Warner <scottbwarner@gmail.com>
2015-07-06 13:21:06 -0400

    change scope

--- js/controllers.js
+++ js/controllers.js
@@ -2,7 +2,7 @@ var myApp = angular.module('myApp', []);
 
 myApp.controller('MyController', function MyController($scope){
 
-	$scope.author = {
+	$scope.characters = {
 		'name': 'Scott',
 		'title': 'Awesome Software Developer',
 		'favColor': 'Yellow'
