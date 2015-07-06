36f90dfa2369b4c50b452abbe59d139098fea65a
Scott Warner <scottbwarner@gmail.com>
2015-07-06 13:23:21 -0400

    create blank css file

--- js/controllers.js
+++ js/controllers.js
@@ -2,10 +2,30 @@ var myApp = angular.module('myApp', []);
 
 myApp.controller('MyController', function MyController($scope){
 
-	$scope.characters = {
-		'name': 'Scott',
-		'title': 'Awesome Software Developer',
-		'favColor': 'Yellow'
+	$scope.characters = 
+{
+	"name":"Dandy Warhol",
+	"shortname":"dandy",
+	"reknown":"Bard",
+	"bio":"the captain of the Pauly Offshore"
+	},
+	{
+	"name":"Ouija Bjord",
+	"shortname":"Ouija",
+	"reknown":"Death Cleric",
+	"bio":"a really weird girl who sells arts and crafts"
+	},
+	{
+	"name":"Karma Le Khayturn",
+	"shortname":"Karma",
+	"reknown":"Monk",
+	"bio":"a very moody person"
+	},
+	{
+	"name":"Chuffy Lickwound",
+	"shortname":"Chuffy",
+	"reknown":"Goblin",
+	"bio":"a sadistic, horrible little thing"
 	};
 
 });
